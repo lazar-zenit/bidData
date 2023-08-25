0eb12eb257ccf607ea33d589c108022119deb4b9
lazar-zenit <62837269+lazar-zenit@users.noreply.github.com>
2023-08-25 22:43:28 +0200

    added gbif cleaner and datasets

---
+++ GBIF_clean&compare/README.md
@@ -0,0 +1,9 @@
+# gbif-cleaner
+
+small piece of code made to select all fungi, delete unesessary columns and delete duplitate
+
+To be used in conjunction with iNat_API (https://github.com/lazar-zenit/iNat_API) for making checklist of Serbia fungi
+
+file "verbatim.txt" is part of DwC-A (Darwin Core Archive) dataset downloaded from gbif.org
+(GBIF.org (06 August 2023) GBIF Occurrence Download https://doi.org/10.15468/dl.wehspq )
+ 
---
+++ GBIF_clean&compare/gbif_cleaner.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+df=pd.read_csv("verbatim.txt", sep="\t", low_memory=False)
+
+columns_to_copy=['gbifID',
+                   'year',
+                   'taxonID',
+                   'scientificName',
+                   'kingdom',
+                   'phylum',
+                   'class',
+                   'order',
+                   'family',
+                   'subfamily',
+                   'genus',
+                   'subgenus',
+                   'taxonRank',
+                   'genericName',
+                   'vernacularName',
+                   'continent',
+                   'country',
+                   'municipality',
+                   'locality',
+                   'decimalLatitude',
+                   'decimalLongitude',
+                   'geodeticDatum']
+
+all_fungi=df.loc[df['kingdom']=='Fungi']
+
+all_fungi_clean=all_fungi[columns_to_copy].copy().sort_values(by=['scientificName']).drop_duplicates(subset=['scientificName'])
+all_fungi_clean.to_excel("all_fungi_gbif_clean.xlsx")
+all_fungi_clean.to_csv("all_fungi_gbif_clean.csv")
+
