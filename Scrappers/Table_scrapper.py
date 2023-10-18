bcca2798cb0920de05a6e8a8c3d165f37fe60ed0
lazar-zenit <62837269+lazar-zenit@users.noreply.github.com>
2023-10-18 21:34:50 +0200

    table scrapper pandas

---
+++ Scrappers/Table_scrapper.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+url='https://pskspartak.rs/mapa-planinarskih-domova-u-srbiji/'
+mh= pd.read_html(url)
+df=pd.DataFrame(mh[0])
+print(df)       
+
+df.to_csv("mountain_house_list.csv")
+
+# beautiful soup code. pandas does this easier
+'''
+r = requests.get(url)
+print(r) #<Response [200]> mean we can scrape the data
+
+soup = BeautifulSoup(r.text, 'html.parser') # parse the html
+table = soup.find("table", class_="tablepress tablepress-id-10 dataTable no-footer") #find the table with said class
+
+print(table)
+for pd in table.find_all('tbody'):0 # find tbody class
+    rows=pd.find_all('tr')
+        for row in rows:
+'''
