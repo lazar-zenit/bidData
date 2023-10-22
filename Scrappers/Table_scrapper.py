25eafb2ecc0b705c00a69c225272a8df24c51600
lazar-zenit <62837269+lazar-zenit@users.noreply.github.com>
2023-10-22 21:07:20 +0200

    .

--- Scrappers/Table_scrapper.py
+++ Scrappers/Table_scrapper.py
@@ -2,12 +2,13 @@ import pandas as pd
 
 url='https://pskspartak.rs/mapa-planinarskih-domova-u-srbiji/'
 mh= pd.read_html(url)
-df=pd.DataFrame(mh[0])
+df=pd.DataFrame(mh[0]) #pd.read_html gives nested list of tables and this time we need first
 print(df)       
 
 df.to_csv("mountain_house_list.csv")
 
 # beautiful soup code. pandas does this easier
+
 '''
 r = requests.get(url)
 print(r) #<Response [200]> mean we can scrape the data
